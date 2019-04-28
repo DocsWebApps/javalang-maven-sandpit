212b6e43e292d051f0ae3e66197b0431fb22920f
Dave Collier <lardelbow@gmail.com>
2019-04-28 19:35:04 +0100

    Initial commit.

---
+++ environment.cmd
@@ -0,0 +1,3 @@
+set JAVA_HOME=c:\MyOracle\Java\jdk-11.0.3
+set MAVEN_HOME=C:\MyOracle\apache-maven-3.6.1
+set PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%PATH%
---
+++ src/main/java/basics/Quiz1.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class Quiz1 {
+    public static void main(String[] args) {
+        for(int i=0; i<1; i=addOne(i)) {
+            System.out.println("T1");
+        }
+    }
+
+    private static int addOne(int i) {
+        System.out.println("T2");
+        return ++i;
+    }
+}
