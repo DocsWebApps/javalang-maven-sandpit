7043ebb6d267276fc043da615775c8e29b24c849
dave.elsby <lardelbow@gmail.com>
2019-12-17 13:26:31 +0000

    Oracle Java Exception Quiz: 18/12/2019

---
+++ src/main/java/JavaQuiz/ExceptionsDeclarationChallenge.java
@@ -0,0 +1,28 @@
+package JavaQuiz;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExceptionsDeclarationChallenge {
+
+    public static void main(String... doYourBest) throws FileNotFoundException {
+        new Scientist() {
+            public void talk() throws FileNotFoundException {
+                super.talk();
+            }
+        }.talk();
+    }
+
+    static class Scientist {
+        public void talk() throws FileNotFoundException { // Line 15
+            System.out.println("Making experiences");
+        }
+    }
+
+    static class Sheldon extends Scientist {
+        public void talk() throws IOException { // Line 21
+            System.out.println("My spot");
+        }
+    }
+
+}
