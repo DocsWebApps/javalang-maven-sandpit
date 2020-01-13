533b53a708cf64d8bd2fc6787bb6c4855556af04
dave.elsby <lardelbow@gmail.com>
2020-01-13 13:44:34 +0000

    JavaQuiz: 13/01/2010

---
+++ src/main/java/JavaQuiz/OptionalChallenge3.java
@@ -0,0 +1,21 @@
+package JavaQuiz;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OptionalChallenge3 {
+
+    public static void main(String... doYourBest) {
+        Optional<String> value = Optional.of("d'oh");
+
+        AtomicInteger dohQtd = new AtomicInteger(999);
+        AtomicInteger eatMyShortsQtd = new AtomicInteger(500);
+
+        value.ifPresentOrElse(
+                v -> dohQtd.incrementAndGet(),
+                eatMyShortsQtd::incrementAndGet);
+
+        System.out.println(value.get() + (dohQtd.get() + eatMyShortsQtd.get()) + " - " + dohQtd.get() + ":" + eatMyShortsQtd.get());
+    }
+
+}
