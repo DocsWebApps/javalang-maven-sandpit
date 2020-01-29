20aac2edd0b08c58e59ebd36161200db90628897
dave.elsby <lardelbow@gmail.com>
2020-01-29 11:31:53 +0000

    JavaQuiz: 29/01/2020

---
+++ src/main/java/JavaQuiz/MapEqualsChallenge.java
@@ -0,0 +1,42 @@
+package JavaQuiz;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class MapEqualsChallenge {
+    public static void main(String... doYourBest) {
+        Map<Stark, String> map = new LinkedHashMap<>();
+
+        map.put(new Stark("Arya"), "1"); map.put(new Stark("Ned"), "2");
+        map.put(new Stark("Sansa"), "3"); map.put(new Stark("Bran"), "4");
+        map.put(new Stark("Jaime"), "5");
+
+        map.forEach((k, v) -> System.out.println(v));
+    }
+    static class Stark {
+        String name;
+        Stark(String name) {this.name = name;}
+        public boolean equals(Object obj) {
+            return ((Stark)obj).name.length() ==
+                    this.name.length(); }
+        public int hashCode() { return 4000 << 2 * 2000 / 10000; }
+    }
+}
+/*
+
+Summary
+You will learn how to use Map with equals and hashcode.
+
+Background
+When we use Map, overriding equals and hashcode because then it's possible to put unique keys into the Map.
+In this case, equals and hashcode are being defined with the length of the String. So, objects are going to be the same if the String length is the same.
+Arya has 4 characters - it will be inserted
+Ned has 3 characters - it will be inserted
+Sansa has 5 characters - it will be inserted
+Bran has 4 characters - it will replace Arya
+Jaime has 5 characters - it will replace Sansa
+As Bran has 4 characters and it was the last one to be inserted, it will replace Arya. Therefore the first value is 4.
+As Ned is the only one with 3 characters, his value will be printed that is 2.
+Then, Sansa has 5 characters, it will be inserted and then Jaime will replace Sansa value with 5.
+ */
