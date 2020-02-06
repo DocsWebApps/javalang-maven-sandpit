a0d73284ba60181fc3e9245e3d0c7a9fe1b705de
dave.elsby <lardelbow@gmail.com>
2020-02-06 11:44:51 +0000

    JavaQuiz: 06/02/2020

---
+++ src/main/java/JavaQuiz/JokerCrazinessChallenge.java
@@ -0,0 +1,19 @@
+package JavaQuiz;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class JokerCrazinessChallenge {
+
+    static int jokerCraziness = 0;
+
+    public static void main(String... doYourBest) {
+        Supplier<Integer> supplier = () -> jokerCraziness++;
+
+        Consumer<Integer> consumer = (batmanAttack) ->
+                System.out.println(batmanAttack + jokerCraziness++);
+
+        System.out.println(jokerCraziness + supplier.get());
+        consumer.accept(1);
+    }
+}
