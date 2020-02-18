296d194388bc01ae4b12c775c8fee6307de16925
dave.elsby <lardelbow@gmail.com>
2020-02-18 14:16:19 +0000

    JavaQuiz: 18/02/2020

--- src/main/java/JavaQuiz/OptionalChallenge1.java
+++ src/main/java/JavaQuiz/OptionalChallenge1.java
@@ -48,3 +48,38 @@ public class OptionalChallenge1 {
     }
 
 }
+/*
+Background
+Java 8 introduced the Optional class in java.util package. When using Optional, you are able to specify alternate values to return or to run.
+The result is that is more readable since information about the code that were previously hidden are now visible to developers.
+Here is a detailed explanation of what happens when the main method is invoked:
+
+In the orElse line:
+
+    finalZionValue += Optional.ofNullable(agentSmith).orElse(getVisionFromOracle());
+
+As agentSmith is not null, the orElse method won't be executed,
+but the trick is that the getVisionFromOracle() method will be executed anyway incrementing the matrixCount to 1.
+
+In the orElseGet line:
+
+    finalZionValue += optFromMatrix.orElseGet(OptionalChallenge::getVisionFromOracle);
+
+We are using the orElseGet method that blocks the invocation of the getVisionFromOracle() method invocation. This is the difference between orElse and orElseGet methods.
+
+In the orElseThrow code:
+
+    Optional.ofNullable(neo).orElseThrow(IllegalArgumentException::new);
+
+We are comparing if neo is null, neo is null at this moment so IllegalArgumentException will be thrown.
+
+    finalZionValue += Optional.ofNullable(neo).or(() -> Optional.of("theOne")).get();
+
+As neo is null again, the or method will be invoked returning "theOne".
+
+The last snippet of code will simply transform "trinity" to upper case returning "TRINITY":
+finalZionValue += Optional.of("trinity").stream().map(String::toUpperCase)
+        .filter(trinity -> trinity.equals("TRINITY")).collect(Collectors.joining());
+
+The final output will be: Virus1theOneTRINITY
+ */
