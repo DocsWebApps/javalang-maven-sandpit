001afd4d22a383982e8d785e1767894ac6b3f486
dave.elsby <lardelbow@gmail.com>
2020-02-19 09:54:12 +0000

    JavaQuiz: 18/02/2020

---
+++ src/main/java/JavaQuiz/InterfaceChallenge.java
@@ -0,0 +1,74 @@
+package JavaQuiz;
+
+import java.util.function.Function;
+
+public class InterfaceChallenge {
+
+    interface Jedi {
+        String MASTER = "Yoda";
+
+        // jedi = "Luke" from jump() and is a delimiter in String.join()
+        // userSaber(0)= "S", userForce()="F" therefore result from attack() is "SLukeF"
+        default String attack() {
+            return jump(jedi -> String.join(jedi, useSaber(), useForce()));
+        }
+
+        private String jump(Function<String, String> function) {
+            return function.apply("Luke");
+        }
+
+        private static String useSaber() {
+            return "S";
+        }
+
+        private String useForce() {
+            return "F";
+        }
+
+    }
+
+    public static void main(String... starWars) {
+        System.out.println(new Jedi() {
+            // after attack() = "SLukeF"
+            // after Jedi.userSaber() = "SLukeFS"
+            // after Jedi.MASTER = "SLukeFSYoda"
+            public String useForce() { return "X"; } }.attack() + Jedi.useSaber() + Jedi.MASTER);
+
+        // Doesn't seem to override userForce() in the Interface ??
+        Jedi dave = new Jedi() {private String useForce() { return "X"; }};
+        System.out.println(dave.useForce());
+    }
+}
+
+/*
+Let's analyse this code:
+
+    System.out.println(new Jedi() {
+        public String useForce() {
+            return "X";
+        }
+     }.attack() + Jedi.useSaber() + Jedi.MASTER);
+
+Note that the useForce method is being overridden inside an anonymous inner class.
+An anonymous inner class is basically a class that doesn't have a name implementing in our case the Jedi interface.
+Then we are invoking the attack method.
+
+    default String attack() {
+        return jump(jedi -> String.join(jedi, useSaber(), useForce()));
+    }
+
+There is a trick here in the join method. The first argument of the join method is a delimiter.
+As we are returning Luke in this code:
+
+    private String jump(Function<String, String> function) {
+        return function.apply("Luke");
+    }
+
+Luke will be the delimiter, and the useSaber, useForce methods are going to simply return a String.
+So the first part of the String will be: SLukeF
+
+Then the Jedi.useSaber() method will be invoked, and note that this method is being invoked in a static way.
+So the method we overrode won't be invoked, this method will return S.
+
+Finally, the constant Jedi.MASTER will be printed with the final input: SLukeFSYod
+ */
--- src/main/java/JavaQuiz/OptionalChallenge1.java
+++ src/main/java/JavaQuiz/OptionalChallenge1.java
@@ -9,42 +9,48 @@ public class OptionalChallenge1 {
     // finalZionValue is not NULL, its a blank String !!
     static String finalZionValue = "";
     static int matrixCount = 0;
+    static int intCount = 1;
 
     public static void main(String... matrix) {
         Optional<String> optFromMatrix = Optional.ofNullable(finalZionValue);
 
         var agentSmith = "Virus";
-
+        displayFinalZionValue("1");
         // Although orElse() is not executed, the method getVisionFromOracle() is evaluated
-        // matrixCount gets incremented but finalZionValue stays as "Virus" for some fucking odd reason !
+        // although matrixCount gets incremented but finalZionValue is assigned
+        // as "Virus" and not "KeyMakerVirus" for some fucking odd reason !
         finalZionValue += Optional.ofNullable(agentSmith).orElse(getVisionFromOracle());
-        System.out.println("2: " + finalZionValue);
+        displayFinalZionValue("3");
 
         finalZionValue += optFromMatrix.orElseGet(OptionalChallenge1::getVisionFromOracle);
-        System.out.println("3: " + finalZionValue);
+        displayFinalZionValue("4");
 
         finalZionValue += matrixCount;
-        System.out.println("4: " + finalZionValue);
+        displayFinalZionValue("5");
 
         String neo = null;
         try {
             Optional.ofNullable(neo).orElseThrow(IllegalArgumentException::new);
         } catch (Exception exception) {
             finalZionValue += Optional.ofNullable(neo).or(() -> Optional.of("theOne")).get();
-            System.out.println("5: " + finalZionValue);
+            displayFinalZionValue("6");
         }
 
         finalZionValue += Optional.of("trinity").stream().map(String::toUpperCase)
                 .filter(trinity -> trinity.equals("TRINITY")).collect(Collectors.joining());
-        System.out.println("6: " + finalZionValue);
+        displayFinalZionValue("7");
     }
 
     static String getVisionFromOracle() {
         matrixCount++;
         finalZionValue = "KeyMaker";
         System.out.println("getVisionFromOracle() called: matrixCount, finalZionValue are : " + matrixCount + ", " + finalZionValue);
-        System.out.println("1: " + finalZionValue);
+        displayFinalZionValue("2");
         return "Architect";
+    }
+
+    static void displayFinalZionValue(String myOrder) {
+        System.out.println(intCount++ + " : " + myOrder + " : " + finalZionValue);
     }
 
 }
