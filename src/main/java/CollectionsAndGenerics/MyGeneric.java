d79a783fe8ec0f9a8ca087b21cd4085b34822579
Dave Collier <lardelbow@gmail.com>
2020-03-12 20:14:08 +0000

    Added more info

--- src/main/java/CollectionsAndGenerics/MyGeneric.java
+++ src/main/java/CollectionsAndGenerics/MyGeneric.java
@@ -1,4 +1,4 @@
-package Generics;
+package CollectionsAndGenerics;
 import java.util.List;
 
 public class MyGeneric <T> {
@@ -21,31 +21,36 @@ public class MyGeneric <T> {
 	}
 	
 	public static void main(String[] args) {
-		// Generics
-		MyGeneric<String> myString=new MyGeneric<>();
-		MyGeneric<Integer> myInteger=new MyGeneric<>();
-		
-		myString.setT(new String("Hello"));
-		myInteger.setT(new Integer(100));
-		
-//		System.out.println(myString.toString());
-//		System.out.println(myInteger.toString());
-
-		// Basic RAW List Type - No Generics
+		// 1. Basic RAW List Type - No Generics
 		System.out.println("No Generics...");
 		Customer.CustomerService customerService = Customer.service();
 		List customers = customerService.getCustomers();
+//		Customer c = (Customer) customers.get(1); // Have to cast as List has no type information
 
 		customers.forEach(System.out::println);
-
+		// OR
 		for(Object cust : customers) {
 			System.out.println((cust).toString());
 		}
 
-		// Using Generics
+		// 2. Same as above but using generics
 		System.out.println("\nUsing Generics...");
 		List<Customer> customers1 = customerService.getCustomers1();
 		customers.forEach(System.out::println);
+
+		// ---------------------------------------------
+
+		// 3. Using MyGeneric
+		MyGeneric<String> myString=new MyGeneric<>();
+		MyGeneric<Integer> myInteger=new MyGeneric<>();
+		
+		myString.setT(new String("Hello"));
+		myInteger.setT(new Integer(100)); // Intellij is complaining since autoboxing now applies
+		// and the constructor is deprecated
+		myInteger.setT(100);
+		
+		System.out.println(myString.getT());
+		System.out.println(myInteger.getT().toString());
 
 	}
 }
