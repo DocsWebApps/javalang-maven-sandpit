e3759d7234a897796832741e115e71c48b80f821
Dave Collier <lardelbow@gmail.com>
2020-05-10 10:10:54 +0100

    10/05/2020 Java Quiz

--- src/main/java/JavaQuiz/DevGymException.java
+++ src/main/java/JavaQuiz/DevGymException.java
@@ -5,12 +5,14 @@ public class DevGymException {
     private static IllegalArgumentException illegalArgument;
 
     public static void main(String... doYourBest) {
+
         try {
             double exceptionNumber = 2 / 0.0;
             System.out.println(exceptionNumber);
             System.out.println("exceptionNumber");
 
-            throw illegalArgument;
+//            illegalArgument = new IllegalArgumentException();
+            throw illegalArgument; // Null pointer/reference, hasn't been instantiated, see above
         } catch (NullPointerException nullPointer) {
             System.out.println("NullPointer");
         } catch (IllegalArgumentException illegalArgument) {
