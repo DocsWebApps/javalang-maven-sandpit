547f891aac2bc5a9473646ef3187bde2dd8ffe7a
Dave Collier <lardelbow@gmail.com>
2020-05-22 09:06:47 +0100

    21/05/2020 Java Quiz

--- src/main/java/JavaQuiz/MisterBurnsGetsMedicine.java
+++ src/main/java/JavaQuiz/MisterBurnsGetsMedicine.java
@@ -28,3 +28,39 @@ public class MisterBurnsGetsMedicine {
         Smithers provideMedicine(int quantity);
     }
 }
+
+/*
+
+Summary
+You will learn how to manipulate Method References when instantiating a new class and will understand better the concept of higher functions and functional programming with Java.
+
+Background
+The concept of Method Reference in Java is very powerful,
+it enables us to create a function of a function, it's also called as higher-order functions.
+An interesting way to use Method References is with a constructor.
+
+Let's analyze the code:
+
+    MedicineSupplier medicine = Smithers::new;
+
+The first line only works because the interface MedicineSupplier matches exactly with one of the
+constructors from the Smithers class. Let's see it:
+
+    static class Smithers {
+            Smithers(int any) { System.out.printf("Smithers gives:%s to Mister Burns with int \n", any); }
+             // Other constructors...
+    }
+
+    private interface MedicineSupplier {
+         Smithers provideMedicine(int quantity);
+    }
+
+Notice that the constructor from Smithers receives an int and returns Smithers and the MedicineSupplier
+method matches exactly the same. As long as the other constructors don't match the MedicineSupplier
+interface method, only the Smithers(int any) {  ... } constructor will be invoked.
+
+Therefore the result of this challenger will be:
+Smithers gives:3 to Mister Burns with int
+Smithers gives:10 to Mister Burns with int
+Smithers gives:1 to Mister Burns with int
+ */
